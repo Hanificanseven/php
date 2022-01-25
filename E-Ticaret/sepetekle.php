a4ea5cdd158ee34d31b4d0ae21cea34f5d5616e7
Hanificanseven <93478377+Hanificanseven@users.noreply.github.com>
2022-01-25 09:26:12 +0300

    Delete E-Ticaret directory

--- E-Ticaret/sepetekle.php
+++
@@ -1,4 +0,0 @@
-<?php
-$id = $_GET["id"];
-setcookie($id . "-" . rand(0, 10000), $id, time() + 3600);
-header("location:products.php");
--- E-Ticaret/sepettenkaldir.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$id = $_GET['id'];
-setcookie($id, "", time()-3600);
-header("location:cart.php");
